db647954f08a8740c4c18412d7b90ea3a8849105
CodeNovice <codenovice2017@gmail.com>
2021-01-03 15:33:50 +0800

    ReentrantLock Test

--- Codes/juc/src/main/java/juc/App.java
+++ Codes/juc/src/main/java/juc/App.java
@@ -1,6 +1,9 @@
 package juc;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Hello world!
@@ -11,6 +14,7 @@ public final class App {
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
@@ -18,13 +22,16 @@ public final class App {
 
         // testSingletonDemo();
 
-        for (int i = 0; i < 10; i++) {
-            new Thread(()->SingletonDemo.getInstance(),String.valueOf(i)).start();
-        }
+        // 单例模式测试
+        // for (int i = 0; i < 10; i++) {
+        //     new Thread(() -> SingletonDemo.getInstance(), String.valueOf(i)).start();
+        // }
+
+        reentrantLockTest();
     }
 
     // 单例模式测试
-    static void testSingletonDemo(){
+    static void testSingletonDemo() {
         // 这里的 == 是比较内存地址
         System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
         System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
@@ -33,8 +40,58 @@ public final class App {
     }
 
     // 原子类AtomicInteger测试
-    static void atomicIntegerTest(){
+    static void atomicIntegerTest() {
         AtomicInteger atomicInteger = new AtomicInteger();
         atomicInteger.compareAndSet(5, 2020);
+    }
+
+    // ReentrantLock可重入测试
+    static void reentrantLockTest() {
+        Phone phone = new Phone();
+        new Thread(()->{
+            phone.get();
+        }, "t1").start();
+        new Thread(() -> {
+            phone.get();
+        }, "t2").start();
+        new Thread(() -> {
+            phone.get();
+        }, "t3").start();
+    }
+    // ReentrantLock可重入测试所需
+    public static class Phone implements Runnable {
+
+        private Lock lock = new ReentrantLock(false);
+
+        public void get(){
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t get Lock");
+                set();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }finally{
+                lock.unlock();
+            }
+        }
+
+        public void set() {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t set Lock");
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void run() {
+            get();
+        }
+    }
+
+    // while循环 + compareAndSet实现自旋锁
+    static void whileAndCASImplementsLock(){
+        AtomicReference<Thread> atomicReference = new AtomicReference<>();
     }
 }
