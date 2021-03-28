e17f556c09a017399e7f2b303f89389dceabeba5
CodeNovice <codenovice2017@gmail.com>
2021-03-28 11:20:55 +0800

    GC root Demo Finished

--- Codes/jvm/src/main/java/JVM/GCRootDemo.java
+++ Codes/jvm/src/main/java/JVM/GCRootDemo.java
@@ -1,19 +1,37 @@
 package JVM;
 
-
+import java.util.HashMap;
 
 public class GCRootDemo {
 
     // 方法区中类的静态属性引用的对象
     // static静态是全部实例共用,会被加载进入方法区(jdk1.7用永久代实现,jdk1.8用元空间实现)
     // 这种也称为引用可达
-    private static GCRootDemo2 gcRootDemo2;
+    // private static GCRootDemo2 gcRootDemo2;
 
     // 方法区中常量引用的对象
-    private static GCRootDemo3 gcRootDemo3 = new GCRootDemo3(0);
+    // private static GCRootDemo3 gcRootDemo3 = new GCRootDemo3(0);
 
     public static void m1(){
         GCRootDemo gcRootDemo = new GCRootDemo();
+
+        int c = 0;
+        c = Integer.MIN_VALUE + -1;
+        System.out.println(c);
+
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1,null);
+
+        Thread t1 = new Thread(new Runnable(){
+            @Override
+            public void run(){
+                System.out.println("哈哈");
+            }
+        });
+
+        t1.start();
+        t1.start();
+
         System.gc();
         System.out.println("第一次GC完成");
     }
