d9538aed1f0f0bec77cc7e6b5ff05658916f68d6
CodeNovice <codenovice2017@gmail.com>
2021-06-10 16:30:00 +0800

    JVM Test

--- Codes/jvm/src/test/java/JVM/AppTest.java
+++ Codes/jvm/src/test/java/JVM/AppTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
  * Unit test for simple App.
  */
@@ -14,5 +17,13 @@ class AppTest {
     @Test
     void testApp() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    void testOptional(){
+        int[] nums = new int[10];
+        Arrays.fill(nums, 10);
+        Optional<Integer> sum = Arrays.stream(nums).boxed().reduce((x,y) -> x + y);
+        System.out.println(sum.get());
     }
 }
